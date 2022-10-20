5e61e340af99f3abf7760038e50b9ce81ffee2ab
Damian Oportus <coportus99@gmail.com>
2022-10-20 16:23:08 -0400

    Debugging these noobs

--- app/src/main/java/com/example/applicationcuisiner/MainActivity.java
+++ app/src/main/java/com/example/applicationcuisiner/MainActivity.java
@@ -49,10 +49,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view){
             startActivity(new Intent(getApplicationContext(),ClientRegistrationActivity.class));
-
-            }
-
-        }
+            }});
     }
 
     /**
