c46ef85434d3eabfeaacbc2efccb5cda086cb83a
Carogg28 <90726597+Carogg28@users.noreply.github.com>
2022-10-20 17:47:24 -0400

    Revert "Say Welcome + className"
    This reverts commit a85318038aa22e91752f3509bb87e8f4bd467716.

--- app/src/main/java/com/example/applicationcuisiner/SecondActivity.java
+++ app/src/main/java/com/example/applicationcuisiner/SecondActivity.java
@@ -8,10 +8,6 @@ import android.view.View;
 
 public class SecondActivity extends AppCompatActivity {
 
-    public SecondActivity(Object object) {
-        System.out.println("Welcome "+object.getClass().getSimpleName());
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
