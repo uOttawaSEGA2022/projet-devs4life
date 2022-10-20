14532bb6bc52b8b0529fe4a25416159a161f43fd
Carogg28 <90726597+Carogg28@users.noreply.github.com>
2022-10-20 18:19:08 -0400

    GRACIAS

--- app/src/main/java/com/example/applicationcuisiner/MainActivity.java
+++ app/src/main/java/com/example/applicationcuisiner/MainActivity.java
@@ -45,14 +45,7 @@ public class MainActivity extends AppCompatActivity {
 
         myRef.setValue("Hello, World!");
 
-        clientRegister.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-            startActivity(new Intent(getApplicationContext(),ClientRegistrationActivity.class));
-
-            }
-
-        }
+    
     }
 
     /**
