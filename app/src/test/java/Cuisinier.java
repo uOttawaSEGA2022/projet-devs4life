2bcbdf9eb8b6ee7aeb76975c9a55eb988de289f2
Ari0549 <91145371+Ari0549@users.noreply.github.com>
2022-10-20 16:31:56 -0400

    Ajout de la classe Cuisinier pour que l'utilisateur puisse s'inscrire en tant que cuisinier.

---
+++ app/src/test/java/Cuisinier.java
@@ -0,0 +1,16 @@
+public class Cuisinier extends User {
+
+
+    protected String address;
+    // ajouter variable pour chèque
+
+    public String getAddress () {
+
+        return address;
+    }
+
+    public void setAddress (String address) {
+
+        this.address = address;
+    }
+}
