8f174a5eeff0056235ba80ffcd96b2755fc1beba
Ari0549 <91145371+Ari0549@users.noreply.github.com>
2022-10-21 12:34:26 -0400

    Ajout de commentaires

--- app/src/test/java/Cuisinier.java
+++ app/src/test/java/Cuisinier.java
@@ -1,3 +1,10 @@
+/**
+ * La classe Cuisinier étend la classe abstraite User et
+ * a pour objectif de permettre d'enregistrer d'informations
+ * propres aux utilisateurs cuisiniers.
+ * @author Chloé Al-Frenn
+ */
+
 public class Cuisinier extends User{
     protected String address;
     //getter
