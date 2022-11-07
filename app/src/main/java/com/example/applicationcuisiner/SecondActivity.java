4b08ee891f35154e9115a685e42eda5892422ac4
Chloe Al-Frenn <chloealfrenn@hotmail.com>
2022-11-06 22:19:12 -0500

    Modification de la classe SecondActivity.java j'ai ajouter le timer qui pour le moment est de 1 minute lorsque l'utilisateur est "Banned" a la fin du timer son status change pour actif alors svp rechangez le manuellement par la suite afin qu'on puisse le reutiliser

--- app/src/main/java/com/example/applicationcuisiner/SecondActivity.java
+++ app/src/main/java/com/example/applicationcuisiner/SecondActivity.java
@@ -1,11 +1,14 @@
 package com.example.applicationcuisiner;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
 import androidx.annotation.NonNull;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
@@ -13,11 +16,16 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
 
 
 public class SecondActivity extends AppCompatActivity {
@@ -26,6 +34,7 @@ public class SecondActivity extends AppCompatActivity {
     private String currentUserID;
     private String type;
     private String status;
+    long duration; //timer duration
 
 
 
@@ -59,6 +68,49 @@ public class SecondActivity extends AppCompatActivity {
                                 bienvenue.setText("Malheureusement vous etes banni de maniere permanente, vous n'aurez plus acces au reste de l'application");
                             } else if (status != null && status.equals("Banned")){
                                 bienvenue.setText("Malheureusement vous etes banni pour:");
+
+                                //initialize timer duration
+                                duration = TimeUnit.MINUTES.toMillis(1);
+
+                                //initialize coundown timer
+                                new CountDownTimer(duration, 1000) {
+
+                                    @Override
+                                    public void onTick(long millisUntilFinished) {
+                                        //When tick convert milliseconds to minutes and second
+
+                                        String sDuration = String.format(Locale.ENGLISH, "%02d : %02d", TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished), TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished));
+                                   //set converted string on text view
+                                        bienvenue.setText("Malheureusement vous etes banni pour: " + sDuration);
+                                    }
+
+                                    @Override
+                                    public void onFinish() {
+                                        //hidetext view
+                                        bienvenue.setVisibility(View.GONE);
+                                        Toast.makeText(getApplicationContext(), "Countdown timer had ended", Toast.LENGTH_LONG).show();
+                                       //set the status back to active
+                                        fireStore.collection("user").document(currentUserID).update("Status", "Active").addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                    @Override
+                                                    public void onSuccess(Void aVoid) {
+                                                        Log.d(TAG, "DocumentSnapshot successfully updated!");
+                                                    }
+                                                })
+                                                .addOnFailureListener(new OnFailureListener() {
+                                                    @Override
+                                                    public void onFailure(@NonNull Exception e) {
+                                                        Log.w(TAG, "Error updating document", e);
+                                                    }
+                                                });
+
+
+                                    }
+                                }.start();
+
+
+
+
+
                             } else {
                                 bienvenue.setText("Bienvenue vous etes un "+type);
                                 //start cuisinier activity
