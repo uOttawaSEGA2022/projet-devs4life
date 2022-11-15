23e4b5287dc82da61cf3889a4c9cdfee4da585d1
Chloe Al-Frenn <chloealfrenn@hotmail.com>
2022-11-15 18:21:24 -0500

    timer pour cuisinier banni fonctionne

--- app/src/main/java/com/example/applicationcuisiner/CuisinierTemporarilyBannedActivity.java
+++ app/src/main/java/com/example/applicationcuisiner/CuisinierTemporarilyBannedActivity.java
@@ -1,31 +1,57 @@
 package com.example.applicationcuisiner;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.DocumentReference;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
 
 public class CuisinierTemporarilyBannedActivity extends AppCompatActivity {
 
     private FirebaseFirestore fireStore;
     private String currentUserID;
+    TextView countdownText;
+    long duration; //timer duration
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cuisinier_temporarily_banned);
 
-        fireStore = FirebaseFirestore.getInstance();
-        currentUserID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        countdownText = findViewById(R.id.tv_coutdown);
 
+        duration = TimeUnit.MINUTES.toMillis(1);
 
-        // Gets user document from Firestore as reference
-        DocumentReference docRef = fireStore.collection("user").document(currentUserID);
+        long timeBannedInMili = 2000000;
+
+        new CountDownTimer(timeBannedInMili, 1000) {
+
+                public void onTick(long millisUntilFinished) {
+                    countdownText.setText("Time left for the ban: " + String.format("%02d:%02d",
+                            TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished),
+                            TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished) -
+                                    TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished))));
+                }
+
+                public void onFinish() {
+                    countdownText.setText("done!");
+                }
+            }.start();
+
     }
 
 
