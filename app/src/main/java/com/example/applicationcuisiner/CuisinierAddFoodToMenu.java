b78274f7e23dc3010da133d06b0f0306e8fd0030
Chloe Al-Frenn <chloealfrenn@hotmail.com>
2022-11-15 22:22:49 -0500

    nom du cuistot ajouter

--- app/src/main/java/com/example/applicationcuisiner/CuisinierAddFoodToMenu.java
+++ app/src/main/java/com/example/applicationcuisiner/CuisinierAddFoodToMenu.java
@@ -10,12 +10,16 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
@@ -32,6 +36,9 @@ public class CuisinierAddFoodToMenu extends AppCompatActivity {
     DatabaseReference databaseMenu;
     Button addBtn;
     FirebaseFirestore db;
+    String currentUserID;
+    String name;
+    String lastname;
 
 
     @Override
@@ -39,6 +46,24 @@ public class CuisinierAddFoodToMenu extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_food);
          db = FirebaseFirestore.getInstance();
+
+        currentUserID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+
+        DocumentReference docRef = db.collection("user").document(currentUserID);
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document != null) {
+                        name = document.getString("Name");
+                        lastname = document.getString("LastName");
+                    }
+                }
+            }
+        });
+
          EditText foodName = findViewById(R.id.foodName);
          EditText foodDescription = findViewById(R.id.foodDescription);
          EditText foodPrice = findViewById(R.id.foodPrice);
@@ -47,6 +72,7 @@ public class CuisinierAddFoodToMenu extends AppCompatActivity {
          addBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 String FoodName = foodName.getText().toString();
                 String FoodDescription = foodDescription.getText().toString();
                 String FoodPrice = foodPrice.getText().toString();
@@ -56,6 +82,7 @@ public class CuisinierAddFoodToMenu extends AppCompatActivity {
                 menu.put("Food Description", FoodDescription);
                 menu.put("Food Price", FoodPrice);
                 menu.put("Type Of Food", TypeOfFood);
+                menu.put("Name of cook", name + " " + lastname);
 
                 db.collection("menu")
                         .add(menu)
