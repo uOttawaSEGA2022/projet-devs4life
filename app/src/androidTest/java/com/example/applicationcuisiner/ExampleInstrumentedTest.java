58dd5a26c19f9394364118cecf055a6063e3cc2c
Damian Oportus <coportus99@gmail.com>
2022-11-17 18:58:57 -0500

    4 Tests done

--- app/src/androidTest/java/com/example/applicationcuisiner/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/applicationcuisiner/ExampleInstrumentedTest.java
@@ -1,10 +1,20 @@
 package com.example.applicationcuisiner;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isClickable;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.supportsInputMethods;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
 import android.content.Context;
 
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.filters.LargeTest;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -16,11 +26,32 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
+@LargeTest
 public class ExampleInstrumentedTest {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
     @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.applicationcuisiner", appContext.getPackageName());
+    public void checkFoodie() {
+        onView(withText("FOODIE")).check(matches(isDisplayed()));
+
     }
+    @Test
+    public void isConnectionClickable() {
+        onView(withText("CONNEXION")).check(matches(isClickable()));
+    }
+    @Test
+    public void isRegistrationClientClickable() {
+        onView(withText("S'INSCRIRE EN TANT QUE CLIENT")).check(matches(isClickable()));
+    }
+    @Test
+    public void isRegistrationCookClickable() {
+        onView(withText("S'INSCRIRE EN TANT QUE CUISINIER")).check(matches(isClickable()));
+    }
+
+
+
+
 }
