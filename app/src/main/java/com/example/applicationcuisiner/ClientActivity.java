d48b488a342c2204611d1f5a0c59c20f8e4210f3
Chloe Al-Frenn <chloealfrenn@hotmail.com>
2022-12-06 15:25:29 -0500

    Client peut voir les repas selon critere de recherche et seulement si le cuisinier est actif

--- app/src/main/java/com/example/applicationcuisiner/ClientActivity.java
+++ app/src/main/java/com/example/applicationcuisiner/ClientActivity.java
@@ -9,6 +9,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -32,8 +33,12 @@ public class ClientActivity extends AppCompatActivity {
     FirebaseFirestore db;
     ListView repasLV;
     ArrayList<Repas> repasPropArrayList;
-    String fullcookname;
     String documentID;
+    EditText criteres;
+    String recherche;
+
+
+
 
 
     @Override
@@ -45,63 +50,66 @@ public class ClientActivity extends AppCompatActivity {
 
         repasLV = findViewById(R.id.lv_seeMenuClient);
         repasPropArrayList = new ArrayList<>();
+        criteres = findViewById(R.id.et_CritereDeRecherche);
 
     }
 
     public void onClickBtnCuisine(View view) {
+        repasPropArrayList.clear();
         loadDatainListviewForCuisine();
+
+
     }
 
 
+    public void onClickBtnRepas(View view) {
+        repasPropArrayList.clear();
+        loadDatainListviewForRepas();
+
+    }
 
     public void loadDatainListviewForCuisine() { //called in the on Click
-        // below line is use to get data from Firebase
-        // firestore using collection in android.
         db.collection("repasProp").get()
                 .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
                     @Override
                     public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
-                        // after getting the data we are calling on success method
-                        // and inside this method we are checking if the received
-                        // query snapshot is empty or not.
+
                         if (!queryDocumentSnapshots.isEmpty()) {
-                            // if the snapshot is not empty we are hiding
-                            // our progress bar and adding our data in a list.
+
                             List<DocumentSnapshot> list = queryDocumentSnapshots.getDocuments();
                             for (DocumentSnapshot d : list) {
-                                // after getting this list we are passing
-                                // that list to our object class.
-                                Repas repas= d.toObject(Repas.class);
-                                // after getting data from Firebase we are
-                                // storing that data in our array list
-
-                                fullcookname = repas.getCook();
-
-                                CollectionReference citiesRef = db.collection("user");
-                                Query query = citiesRef.whereEqualTo("FullName", fullcookname);
-
+                                Repas repas = d.toObject(Repas.class);
+                                //tri par type de cuisine
+                                recherche = criteres.getText().toString();
+                                if (recherche.equals(repas.getTypeDeCuisine())){
+                                //tri par status du cuisinier
+                                CollectionReference cookRef = db.collection("user");
+                                Query query = cookRef.whereEqualTo("FullName", repas.getCook());
                                 query.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                                     @Override
                                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
                                         if (task.isSuccessful()) {
                                             for (QueryDocumentSnapshot document : task.getResult()) {
-                                                System.out.println("the id of this cook:"+ fullcookname +"is " +document.getId());
+                                                System.out.println("the id of this cook:" + repas.getCook() + "is " + document.getId());
                                                 documentID = document.getId();
-
                                                 DocumentReference docRef = db.collection("user").document(documentID);
                                                 docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
                                                     @Override
                                                     public void onComplete(@NonNull Task<DocumentSnapshot> task) {
                                                         if (task.isSuccessful()) {
-                                                            // Document found in the offline cache
+
                                                             DocumentSnapshot document = task.getResult();
                                                             String status = document.getString("Status");
-                                                            if(status.equals("Active"))
-                                                                repasPropArrayList.add(repas); //adds all the repas of the active cuisinier
-                                                            //TODO: check if the criteria matches from the edittext
-                                                            Log.d(TAG, "Cached document data: " + document.getData());
-                                                        } else {
-                                                            Log.d(TAG, "Cached get failed: ", task.getException());
+
+                                                            System.out.println("status of this cook " + repas.getCook() + "for this dish " + repas.getName() + " is " + status.equals("Active"));
+
+                                                           if (status.equals("Active")) {
+                                                               System.out.println("were in the if statement");
+                                                              repasPropArrayList.add(repas);
+                                                               RepasListAdapterforClient adapter = new RepasListAdapterforClient(ClientActivity.this, repasPropArrayList);
+                                                               repasLV.setAdapter(adapter);
+
+                                                           }
                                                         }
                                                     }
                                                 });
@@ -111,112 +119,78 @@ public class ClientActivity extends AppCompatActivity {
                                     }
                                 });
 
+                                }
                             }
-                            // after that we are passing our array list to our adapter class.
-                            RepasListAdapterforRepas adapter = new RepasListAdapterforRepas(ClientActivity.this, repasPropArrayList);
-
-                            // after passing this array list to our adapter
-                            // class we are setting our adapter to our list view.
-                            repasLV.setAdapter(adapter);
-
-
-
-
 
                         } else {
-                            // if the snapshot is empty we are displaying a toast message.
                             Toast.makeText(ClientActivity.this, "No data found in Database", Toast.LENGTH_SHORT).show();
                         }
                     }
                 }).addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
-                        // we are displaying a toast message
-                        // when we get any error from Firebase.
                         Toast.makeText(ClientActivity.this, "Fail to load data..", Toast.LENGTH_SHORT).show();
                     }
                 });
     }
 
+
     public void loadDatainListviewForRepas() { //called in the on Click
-        // below line is use to get data from Firebase
-        // firestore using collection in android.
         db.collection("repasProp").get()
                 .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
                     @Override
                     public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
-                        // after getting the data we are calling on success method
-                        // and inside this method we are checking if the received
-                        // query snapshot is empty or not.
+
                         if (!queryDocumentSnapshots.isEmpty()) {
-                            // if the snapshot is not empty we are hiding
-                            // our progress bar and adding our data in a list.
+
                             List<DocumentSnapshot> list = queryDocumentSnapshots.getDocuments();
                             for (DocumentSnapshot d : list) {
-                                // after getting this list we are passing
-                                // that list to our object class.
-                                Repas repas= d.toObject(Repas.class);
-                                // after getting data from Firebase we are
-                                // storing that data in our array list
+                                Repas repas = d.toObject(Repas.class);
+                                //tri par type de cuisine
+                                recherche = criteres.getText().toString();
 
-                                fullcookname = repas.getCook();
+                                if (recherche.equals(repas.getTypeDeRepas())){
+                                    //tri par status du cuisinier
+                                    CollectionReference cookRef = db.collection("user");
+                                    Query query = cookRef.whereEqualTo("FullName", repas.getCook());
+                                    query.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                            if (task.isSuccessful()) {
+                                                for (QueryDocumentSnapshot document : task.getResult()) {
+                                                    documentID = document.getId();
+                                                    DocumentReference docRef = db.collection("user").document(documentID);
+                                                    docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                                                        @Override
+                                                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                                            if (task.isSuccessful()) {
+                                                                DocumentSnapshot document = task.getResult();
+                                                                String status = document.getString("Status");
+                                                                if (status.equals("Active")) {
+                                                                    repasPropArrayList.add(repas);
+                                                                    RepasListAdapterforClient adapter = new RepasListAdapterforClient(ClientActivity.this, repasPropArrayList);
+                                                                    repasLV.setAdapter(adapter);
 
-                                CollectionReference citiesRef = db.collection("user");
-                                Query query = citiesRef.whereEqualTo("FullName", fullcookname);
-
-                                query.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                                    @Override
-                                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                                        if (task.isSuccessful()) {
-                                            for (QueryDocumentSnapshot document : task.getResult()) {
-                                                System.out.println("the id of this cook:"+ fullcookname +"is " +document.getId());
-                                                documentID = document.getId();
-
-                                                DocumentReference docRef = db.collection("user").document(documentID);
-                                                docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-                                                    @Override
-                                                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                                                        if (task.isSuccessful()) {
-                                                            // Document found in the offline cache
-                                                            DocumentSnapshot document = task.getResult();
-                                                            String status = document.getString("Status");
-                                                            if(status.equals("Active"))
-                                                                repasPropArrayList.add(repas); //adds all the repas of the active cuisinier
-                                                            //TODO: check if the criteria matches from the edittext
-                                                            Log.d(TAG, "Cached document data: " + document.getData());
-                                                        } else {
-                                                            Log.d(TAG, "Cached get failed: ", task.getException());
+                                                                }
+                                                            }
                                                         }
-                                                    }
-                                                });
+                                                    });
 
+                                                }
                                             }
                                         }
-                                    }
-                                });
+                                    });
 
+                                }
                             }
-                            // after that we are passing our array list to our adapter class.
-                            RepasListAdapterforRepas adapter = new RepasListAdapterforRepas(ClientActivity.this, repasPropArrayList);
-
-                            // after passing this array list to our adapter
-                            // class we are setting our adapter to our list view.
-                            repasLV.setAdapter(adapter);
-
-
-
-
 
                         } else {
-                            // if the snapshot is empty we are displaying a toast message.
                             Toast.makeText(ClientActivity.this, "No data found in Database", Toast.LENGTH_SHORT).show();
                         }
                     }
                 }).addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
-                        // we are displaying a toast message
-                        // when we get any error from Firebase.
                         Toast.makeText(ClientActivity.this, "Fail to load data..", Toast.LENGTH_SHORT).show();
                     }
                 });
